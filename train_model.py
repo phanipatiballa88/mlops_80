3d8a30b9de900e4c4910f1f53c0dd3e2545ea4f1
Patiballa <patibp2@medtronic.com>
2025-01-24 13:34:31 +0530

    Fixing test errors

--- train_model.py
+++ train_model.py
@@ -2,6 +2,7 @@
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 import joblib
+import os
 
 # Load the training data
 X_train = pd.read_csv('data/X_train.csv')
@@ -10,6 +11,9 @@ y_train = pd.read_csv('data/y_train.csv')
 # Train a RandomForest model
 model = RandomForestClassifier(random_state=42)
 model.fit(X_train, y_train.values.ravel())
+
+# Create the model directory if it doesn't exist
+os.makedirs('model', exist_ok=True)
 
 # Save the model
 joblib.dump(model, 'model/model.pkl')
